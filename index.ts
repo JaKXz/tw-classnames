8d0a5eb998cb1521fcb476fdcee02de7e33988d1
Jason Kurian <jason@flexpa.com>
2023-09-15 13:56:23 -0400

    feat: export twMerge function as well

--- index.ts
+++ index.ts
@@ -1,8 +1,10 @@
-import {clsx, type ClassValue} from 'clsx';
-import {twMerge} from 'tailwind-merge';
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function classNames(...args: ClassValue[]): string {
   return twMerge(clsx(...args));
 }
+
+export { twMerge };
 
 export default classNames;
